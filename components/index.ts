55632599dc788f323a91d1abe61dca6244136512
Taider <montassartaider002@gmail.com>
2023-10-10 00:58:04 +0200

    Show More

--- components/index.ts
+++ components/index.ts
@@ -7,6 +7,8 @@ import CustomFilter from "./CustomFilter";
 import SearchManufacturer from "./SearchManufacturer";
 import CarCard from "./CarCard";
 import CarDetails from "./CarDetails";
+import ShowMore from "./ShowMore";
+
 export {
   Hero,
   CustomButton,
@@ -17,4 +19,5 @@ export {
   SearchManufacturer,
   CarCard,
   CarDetails,
+  ShowMore,
 };
--- types/index.ts
+++ types/index.ts
@@ -45,3 +45,8 @@ export interface CustomFilterProps {
   title: string;
   option: OptionProps[];
 }
+
+export interface ShowMoreProps {
+  pageNumber: number;
+  isNext: boolean;
+}
